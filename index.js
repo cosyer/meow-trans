9045d594505296d0c49e2413e7aa1bc4f06f2fa1
chenyu <535509852@qq.com>
2018-10-17 01:44:20 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const pinyin = require("pinyin");
+const program = require("commander");
+
+const translate = require("./translator");
+
+program
+  .version("0.0.5")
+  .description("把你的话都变成喵星语吧🐱")
+  .option("-e --emoj", "使用emoj来替代汉字喵")
+  .option("-i --stdin", "传输标准输入流文本而不是命令行参数")
+  .option("-c --clip", "自动复制到剪贴板（文本过大慎用）")
+  .parse(process.argv);
+
+const word = !program.emoj;
+const clip = program.clip;
+
+if (program.stdin) {
+  process.stdin.setEncoding("utf-8");
+
+  process.stdin.on("readable", () => {
+    let chunk = process.stdin.read();
+    if (chunk !== null) {
+      translate(chunk, word, clip);
+    }
+  });
+} else {
+  if (program.args.length > 0) {
+    translate(program.args[0], word, clip);
+    process.stdout.write(`\n`);
+  } else program.outputHelp();
+}
---
+++ translator.js
@@ -0,0 +1,20 @@
+const pinyin = require("pinyin");
+const toClipboard = require("to-clipboard");
+const isChinese = require("is-chinese");
+
+module.exports = function(chunk, word, clip) {
+  let message = "";
+  [...chunk].forEach(val => {
+    let result = pinyin(val, {
+      style: pinyin.STYLE_NORMAL
+    });
+    let content = "";
+    if (isChinese(val))
+      if (word) content = `喵(${result[0]})`;
+      else content = ` 🐱 (${result[0]})`;
+    else content = `${val}`;
+    process.stdout.write(`${content}`);
+    message += content;
+  });
+  if (clip) toClipboard.sync(`${message}`);
+};
